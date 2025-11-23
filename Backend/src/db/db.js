ed6fdbca23e96a75608a1cec44b637d5e9ff0149
ChetanJoshi1511 <chetanjoshi1511@gmail.com>
2025-11-24 02:09:53 +0530

    Changed single exports to default export

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,10 +1,9 @@
 import mongoose, { connect } from "mongoose";
-import express from "express";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async ()=>{
     try{
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance = await connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log("connected to database",connectionInstance.connection.host);
     } catch(err){
         console.log("Database connection error! ",err);
--- Backend/src/utils/cloudinary.js
+++ Backend/src/utils/cloudinary.js
@@ -23,5 +23,5 @@ const uploadToCloudinary  = async (localFilePath)=>{
         return null;
     }
 }
-
-export {uploadToCloudinary};
+  
+export default uploadToCloudinary;
